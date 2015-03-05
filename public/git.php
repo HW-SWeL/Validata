44521cd21acbf5712bdf4690cab279623a1f98be
Andrew Beveridge <github@andrewbeveridge.co.uk>
2015-03-05 15:16:27 +0000

    added npm and bower update to git webhook

--- public/git.php
+++ public/git.php
@@ -1,4 +1,10 @@
 <?php 
 
 echo `git fetch --all 2>&1`; 
-echo `git reset --hard origin/master  2>&1`; ?>
+echo `git reset --hard  2>&1`; 
+echo `git pull 2>&1`;
+
+echo `npm update 2>&1`;
+echo `bower update 2>&1`;
+
+?>
