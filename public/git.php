b0ad4a979d6cf750e0286399e223bfe0bf8c0194
Andrew Beveridge <github@andrewbeveridge.co.uk>
2015-03-05 15:41:44 +0000

    still fixing webhook

--- public/git.php
+++ public/git.php
@@ -5,11 +5,9 @@ echo "<pre>";
 echo "\n\nwhoami\n";
 echo `whoami 2>&1`;
 
-echo "\n\nHOME="/tmp/" \n";
-echo `HOME="/tmp/" 2>&1`;
-
 echo "\n\ncd ..\n";
 echo `cd .. 2>&1`;
+echo `pwd 2>&1`;
 
 echo "\n\ngit fetch --all\n";
 echo `git fetch --all 2>&1`;
@@ -23,7 +21,7 @@ echo `git pull 2>&1`;
 echo "\n\nnpm update \n";
 echo `npm update 2>&1`;
 
-echo "\n\nbower update \n";
-echo `bower update 2>&1`;
+echo "\n\nHOME=\"/tmp/\" && bower update \n";
+echo `HOME="/tmp/" && bower update 2>&1`;
 
 ?>
