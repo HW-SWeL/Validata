b589f4d0b07273c85480b434a3dfe817a491e278
Andrew Beveridge <github@andrewbeveridge.co.uk>
2015-03-05 15:33:04 +0000

    made webhook more verbose

--- public/git.php
+++ public/git.php
@@ -1,10 +1,20 @@
 <?php 
 
+echo "<pre>";
+
+echo "git fetch --all\n";
 echo `git fetch --all 2>&1`; 
+
+echo "\n\ngit reset --hard\n"; 
 echo `git reset --hard  2>&1`; 
+
+echo "\n\ngit pull \n";
 echo `git pull 2>&1`;
 
+echo "\n\nnpm update \n";
 echo `npm update 2>&1`;
+
+echo "\n\nbower update \n";
 echo `bower update 2>&1`;
 
 ?>
