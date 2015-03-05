36844fda1c0c7114653fc8838452481ab8327498
Andrew Beveridge <github@andrewbeveridge.co.uk>
2015-03-05 15:45:09 +0000

    webhook fix

--- public/git.php
+++ public/git.php
@@ -2,11 +2,10 @@
 
 echo "<pre>";
 
-echo "\n\nwhoami\n";
-echo `whoami 2>&1`;
+chdir ('../');
 
-echo "\n\ncd ..\n";
-echo `cd .. 2>&1`;
+echo "\n\nwhoami, pwd\n";
+echo `whoami 2>&1`;
 echo `pwd 2>&1`;
 
 echo "\n\ngit fetch --all\n";
@@ -21,7 +20,7 @@ echo `git pull 2>&1`;
 echo "\n\nnpm update \n";
 echo `npm update 2>&1`;
 
-echo "\n\nHOME=\"/tmp/\" && bower update \n";
-echo `HOME="/tmp/" && bower update 2>&1`;
+echo "\n\nbower update \n";
+echo `bower update 2>&1`;
 
 ?>
