d787f72038707c00cc2f76293d86c8947760de42
Alasdair Gray <A.J.G.Gray@hw.ac.uk>
2015-07-15 16:53:13 +0100

    Reverted code that changed the default strictness level as it wasn't working

--- public/javascripts/UI.js
+++ public/javascripts/UI.js
@@ -335,17 +335,11 @@ UI = {
         if ( Util.iterableLength( UI.reqLevels ) )
         {
             UI.reqLevelSelector.empty();
-            reqLevelString = "";
+
             UI.reqLevels.forEach(function (reqLevel)
             {
-                reqLevelString = reqLevelString.concat('<option>' + Util.stringValue(reqLevel).toUpperCase() + '</option>');
+                UI.reqLevelSelector.append('<option>' + Util.stringValue(reqLevel).toUpperCase() + '</option>');
             });
-            n = reqLevelString.lastIndexOf("<option>");
-            if (n != -1)
-            {
-              reqLevelString = reqLevelString.substr(0, n+7) + " selected" + reqLevelString.substr(n+7);
-            }
-            UI.reqLevelSelector.append(reqLevelString);
             UI.reqLevelPanel.show();
         }
         else
